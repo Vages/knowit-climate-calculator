61f32cd4b082f646080dbfa2750d3e3be2f299c6
Eirik Vågeskar <eirik.vageskar@protonmail.com>
2022-01-10 14:02:51 +0100

    :recycle: Use camel casing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,79 +6,78 @@
   const MAX_VACATION_DAYS = 25;
   const MAX_DAYS_EVERY_FOUR_WEEKS = 20;
 
-  let kilometers_from_home_to_work = 2.6;
+  let kilometersFromHomeToWork = 2.6;
 
-  let months_without_motorized_vehicles = 3;
-  $: unmotorized_ratio = months_without_motorized_vehicles / 12;
+  let monthsWithoutMotorizedVehicles = 3;
+  $: unmotorizedRatio = monthsWithoutMotorizedVehicles / 12;
 
-  let used_vacation_days = MAX_VACATION_DAYS;
+  let usedVacationDays = MAX_VACATION_DAYS;
 
-  let planned_home_office_days = 0;
+  let plannedHomeOfficeDays = 0;
 
-  $: days_in_office_every_four_weeks =
-    MAX_DAYS_EVERY_FOUR_WEEKS - planned_home_office_days;
+  $: daysInOfficeEveryFourWeeks =
+    MAX_DAYS_EVERY_FOUR_WEEKS - plannedHomeOfficeDays;
 
-  $: in_office_ratio =
-    days_in_office_every_four_weeks / MAX_DAYS_EVERY_FOUR_WEEKS;
+  $: inOfficeRatio =
+    daysInOfficeEveryFourWeeks / MAX_DAYS_EVERY_FOUR_WEEKS;
 
-  let unplanned_absence_days = 0;
+  let unplannedAbsenceDays = 0;
 
-  let rail_days = 0;
-  let bus_days = 0;
+  let railDays = 0;
+  let busDays = 0;
 
   const TWEENED_OPTIONS = { easing: cubicOut };
 
-  $: car_days = days_in_office_every_four_weeks - rail_days - bus_days;
-  let rail_days_tweened = tweened(0, TWEENED_OPTIONS);
-  let bus_days_tweened = tweened(0, TWEENED_OPTIONS);
-  let car_days_tweened = tweened(
+  $: carDays = daysInOfficeEveryFourWeeks - railDays - busDays;
+  let railDaysTweened = tweened(0, TWEENED_OPTIONS);
+  let busDaysTweened = tweened(0, TWEENED_OPTIONS);
+  let carDaysTweened = tweened(
     1, // Avoid blinking on page load
     TWEENED_OPTIONS
   );
 
-  $: $rail_days_tweened = rail_days;
-  $: $bus_days_tweened = bus_days;
-  $: $car_days_tweened = car_days;
+  $: $railDaysTweened = railDays;
+  $: $busDaysTweened = busDays;
+  $: $carDaysTweened = carDays;
 
-  $: rail_ratio = rail_days / days_in_office_every_four_weeks;
-  $: bus_ratio = bus_days / days_in_office_every_four_weeks;
-  $: car_ratio = car_days / days_in_office_every_four_weeks;
+  $: railRatio = railDays / daysInOfficeEveryFourWeeks;
+  $: busRatio = busDays / daysInOfficeEveryFourWeeks;
+  $: carRatio = carDays / daysInOfficeEveryFourWeeks;
 
-  $: actual_working_days =
-    MAX_WORK_DAYS - used_vacation_days - unplanned_absence_days;
-  $: total_days_in_office = Math.round(actual_working_days * in_office_ratio);
-  $: kilometers_traveled =
-    total_days_in_office * 2 * kilometers_from_home_to_work;
+  $: actualWorkingDays =
+    MAX_WORK_DAYS - usedVacationDays - unplannedAbsenceDays;
+  $: totalDaysInOffice = Math.round(actualWorkingDays * inOfficeRatio);
+  $: kilometersTraveled =
+    totalDaysInOffice * 2 * kilometersFromHomeToWork;
 
-  let unmotorized_kilometers, rail_kilometers, bus_kilometers, car_kilometers;
-  $: unmotorized_kilometers = kilometers_traveled * unmotorized_ratio;
-  $: motorized_kilometers = kilometers_traveled - unmotorized_kilometers;
+  $: unmotorizedKilometers = kilometersTraveled * unmotorizedRatio;
+  $: motorizedKilometers = kilometersTraveled - unmotorizedKilometers;
 
-  $: rail_kilometers = motorized_kilometers * rail_ratio;
-  $: bus_kilometers = motorized_kilometers * bus_ratio;
-  $: car_kilometers = motorized_kilometers * car_ratio;
+  $: railKilometers = motorizedKilometers * railRatio;
+  $: busKilometers = motorizedKilometers * busRatio;
+  $: carKilometers = motorizedKilometers * carRatio;
 
-  let unmotorized_kilometers_tweened = tweened(
-    unmotorized_kilometers,
+  let unmotorizedKilometersTweened = tweened(
+    unmotorizedKilometers,
     TWEENED_OPTIONS
   );
-  let rail_kilometers_tweened = tweened(rail_kilometers, TWEENED_OPTIONS);
-  let bus_kilometers_tweened = tweened(bus_kilometers, TWEENED_OPTIONS);
-  let car_kilometers_tweened = tweened(car_kilometers, TWEENED_OPTIONS);
+  let railKilometersTweened = tweened(railKilometers, TWEENED_OPTIONS);
+  let busKilometersTweened = tweened(busKilometers, TWEENED_OPTIONS);
+  let carKilometersTweened = tweened(carKilometers, TWEENED_OPTIONS);
 
-  $: $unmotorized_kilometers_tweened = unmotorized_kilometers;
-  $: $rail_kilometers_tweened = rail_kilometers;
-  $: $bus_kilometers_tweened = bus_kilometers;
-  $: $car_kilometers_tweened = car_kilometers;
+  $: $unmotorizedKilometersTweened = unmotorizedKilometers;
+  $: $railKilometersTweened = railKilometers;
+  $: $busKilometersTweened = busKilometers;
+  $: $carKilometersTweened = carKilometers;
 
   $: (() => {
     // Prevent a negative amount of days if user adjusts number of days in home office
-    if (rail_days + bus_days > days_in_office_every_four_weeks) {
-      if (rail_days) {
-        rail_days = days_in_office_every_four_weeks - bus_days;
+    if (railDays + busDays > daysInOfficeEveryFourWeeks) {
+      if (railDays) {
+        railDays = daysInOfficeEveryFourWeeks - busDays;
       }
-      if (bus_days) {
-        bus_days = days_in_office_every_four_weeks - rail_days;
+      if (busDays) {
+        busDays = daysInOfficeEveryFourWeeks - railDays;
       }
     }
   })();
@@ -99,7 +98,7 @@
         <input
           id="home_work_distance"
           type="number"
-          bind:value={kilometers_from_home_to_work}
+          bind:value={kilometersFromHomeToWork}
           step="0.1"
         />
       </div>
@@ -115,13 +114,13 @@
         </label>
       </div>
       <div>
-        <span class="slider-display">{months_without_motorized_vehicles}</span>
+        <span class="slider-display">{monthsWithoutMotorizedVehicles}</span>
         <input
           id="walk_bike_months"
           type="range"
           min="0"
           max="12"
-          bind:value={months_without_motorized_vehicles}
+          bind:value={monthsWithoutMotorizedVehicles}
           step="1"
         />
       </div>
@@ -134,7 +133,7 @@
         <input
           id="used_vacation_days"
           type="number"
-          bind:value={used_vacation_days}
+          bind:value={usedVacationDays}
           step="1"
           min="0"
           max=""
@@ -149,7 +148,7 @@
         <input
           id="days_in_office_every_four_weeks"
           type="number"
-          bind:value={planned_home_office_days}
+          bind:value={plannedHomeOfficeDays}
           step="1"
           min="0"
           max="20"
@@ -164,7 +163,7 @@
         <input
           id="unplanned_absence_days"
           type="number"
-          bind:value={unplanned_absence_days}
+          bind:value={unplannedAbsenceDays}
           step="1"
           min="0"
         />
@@ -173,9 +172,9 @@
 
     <h3>Fordeling av kontordager</h3>
     <p>
-      Fordi du oppgir at du gjennomsnittlig bruker {planned_home_office_days} av
+      Fordi du oppgir at du gjennomsnittlig bruker {plannedHomeOfficeDays} av
       {MAX_DAYS_EVERY_FOUR_WEEKS} dager på hjemmekontor, regner vi med du bruker
-      {days_in_office_every_four_weeks} av {MAX_DAYS_EVERY_FOUR_WEEKS} dager på kontoret.
+      {daysInOfficeEveryFourWeeks} av {MAX_DAYS_EVERY_FOUR_WEEKS} dager på kontoret.
       <strong>
         Gitt at du ikke må ta ut noe uplanlagt fravær og ikke går/sykler til
         jobb, hvordan fordeler dagene dine seg på ulike reisemidler?
@@ -197,14 +196,14 @@
     </p>
 
     <div class="ratio_display">
-      {#if $rail_days_tweened}
-        <div class="rail" style="flex-grow: {$rail_days_tweened}">Skinner</div>
+      {#if $railDaysTweened}
+        <div class="rail" style="flex-grow: {$railDaysTweened}">Skinner</div>
       {/if}
-      {#if $bus_days_tweened}
-        <div class="bus" style="flex-grow: {$bus_days_tweened}">Buss</div>
+      {#if $busDaysTweened}
+        <div class="bus" style="flex-grow: {$busDaysTweened}">Buss</div>
       {/if}
-      {#if $car_days_tweened}
-        <div class="car" style="flex-grow: {$car_days_tweened}">Bil</div>
+      {#if $carDaysTweened}
+        <div class="car" style="flex-grow: {$carDaysTweened}">Bil</div>
       {/if}
     </div>
 
@@ -214,13 +213,13 @@
         <label for="tram_days">Tog/trikk/Bybane/T-bane</label>
       </div>
       <div>
-        <span class="slider-display">{rail_days}</span>
+        <span class="slider-display">{railDays}</span>
         <input
           id="tram_days"
           type="range"
           min="0"
-          max={days_in_office_every_four_weeks - bus_days}
-          bind:value={rail_days}
+          max={daysInOfficeEveryFourWeeks - busDays}
+          bind:value={railDays}
           step="1"
         />
       </div>
@@ -230,13 +229,13 @@
         <label for="bus_days">Buss</label>
       </div>
       <div>
-        <span class="slider-display">{bus_days}</span>
+        <span class="slider-display">{busDays}</span>
         <input
           id="bus_days"
           type="range"
           min="0"
-          max={days_in_office_every_four_weeks - rail_days}
-          bind:value={bus_days}
+          max={daysInOfficeEveryFourWeeks - railDays}
+          bind:value={busDays}
           step="1"
         />
       </div>
@@ -244,37 +243,37 @@
         <span class="dot car" />
         <span>Bildager</span>
       </div>
-      <span>{car_days}</span>
+      <span>{carDays}</span>
     </div>
 
     <div class="results">
       <h2>Resultater</h2>
-      I fjor hadde du totalt {actual_working_days} arbeidsdager ut av {MAX_WORK_DAYS}
-      mulige. Av disse var {total_days_in_office} på kontoret, og medførte {Math.round(
-        kilometers_traveled
+      I fjor hadde du totalt {actualWorkingDays} arbeidsdager ut av {MAX_WORK_DAYS}
+      mulige. Av disse var {totalDaysInOffice} på kontoret, og medførte {Math.round(
+        kilometersTraveled
       )}
       km reise. Disse fordelte seg som følger:
       <div class="ratio_display">
-        {#if $unmotorized_kilometers_tweened}
+        {#if $unmotorizedKilometersTweened}
           <div
             class="unmotorized"
-            style="flex-grow: {$unmotorized_kilometers_tweened}"
+            style="flex-grow: {$unmotorizedKilometersTweened}"
           >
             Gå/sykle
           </div>
         {/if}
-        {#if $rail_kilometers_tweened}
-          <div class="rail" style="flex-grow: {$rail_kilometers_tweened}">
+        {#if $railKilometersTweened}
+          <div class="rail" style="flex-grow: {$railKilometersTweened}">
             Skinner
           </div>
         {/if}
-        {#if $bus_kilometers_tweened}
-          <div class="bus" style="flex-grow: {$bus_kilometers_tweened}">
+        {#if $busKilometersTweened}
+          <div class="bus" style="flex-grow: {$busKilometersTweened}">
             Buss
           </div>
         {/if}
-        {#if $car_kilometers_tweened}
-          <div class="car" style="flex-grow: {$car_kilometers_tweened}">
+        {#if $carKilometersTweened}
+          <div class="car" style="flex-grow: {$carKilometersTweened}">
             Bil
           </div>
         {/if}
@@ -285,22 +284,22 @@
           <span class="dot unmotorized" />
           Gå/sykle
         </div>
-        <div>{Math.round(unmotorized_kilometers)} km</div>
+        <div>{Math.round(unmotorizedKilometers)} km</div>
         <div>
           <span class="dot rail" />
           Tog/trikk/bybane/T-bane
         </div>
-        <div>{Math.round(rail_kilometers)} km</div>
+        <div>{Math.round(railKilometers)} km</div>
         <div>
           <span class="dot bus" />
           Buss
         </div>
-        <div>{Math.round(bus_kilometers)} km</div>
+        <div>{Math.round(busKilometers)} km</div>
         <div>
           <span class="dot car" />
           Bil (uspesifisert type)
         </div>
-        <div>{Math.round(car_kilometers)} km</div>
+        <div>{Math.round(carKilometers)} km</div>
       </div>
     </div>
     <footer>
